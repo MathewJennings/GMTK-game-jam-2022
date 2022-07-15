8ea4f90050adb30c09af8378e0586dc12c9c5c33
Franklin <zheng.franklin@gmail.com>
2022-07-15 14:59:55 -0400

    Add health scripts for player and enemy

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+    public bool shouldDisplay;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        if(shouldDisplay) { }
+        for(int i = 0; i < hearts.Length; i++)
+        {
+
+            //safety dont go over max health
+            if(health > numOfHearts)
+            {
+                health = numOfHearts;
+            }
+
+            if(i<health)
+            {
+                hearts[i].sprite = fullHeart;
+            } else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            } else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
