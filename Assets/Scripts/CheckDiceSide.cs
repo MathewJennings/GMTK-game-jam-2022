824c121b141d27ef6e9765542c192290830649f1
Mathew Jennings <mathew.percussion@gmail.com>
2022-07-16 01:17:40 -0400

    Drag and Drop Dice Rolling
    * Click on dice to move it around and then release
    * Correctly reports the side that landed up once velocity hits zero
    * Reworked Room 1 to be more open

--- Assets/Scripts/CheckDiceSide.cs
+++ Assets/Scripts/CheckDiceSide.cs
@@ -3,35 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckDiceSide : MonoBehaviour {
-    Vector3 diceVelocity;
 
-    void Start () {
-        diceVelocity = TossDice.diceVelocity;
+    private GameObject diceWhoseRollCompleted;
+
+    private void OnEnable () {
+        DiceDragManager.diceRollCompleted += registerDiceRollCompleted;
+    }
+
+    private void OnDisable () {
+        DiceDragManager.diceRollCompleted -= registerDiceRollCompleted;
+    }
+
+    private void registerDiceRollCompleted (GameObject dice) {
+        diceWhoseRollCompleted = dice;
     }
 
     void OnTriggerStay (Collider other) {
-        if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f) {
-            switch (other.gameObject.name) {
-                // NOTE: the case values are the sides that are DOWN. The logged values are the sides that are UP.
-                case "Side 1":
-                    //Debug.Log ("Rolled a 6");
-                    break;
-                case "Side 2":
-                    //Debug.Log ("Rolled a 5");
-                    break;
-                case "Side 3":
-                    //Debug.Log ("Rolled a 4");
-                    break;
-                case "Side 4":
-                    //Debug.Log ("Rolled a 3");
-                    break;
-                case "Side 5":
-                    //Debug.Log ("Rolled a 2");
-                    break;
-                case "Side 6":
-                    //Debug.Log ("Rolled a 1");
-                    break;
-            }
+        if (other.gameObject.transform.parent.gameObject != diceWhoseRollCompleted) {
+            return;
         }
+        GameObject sideThatIsUp = other.GetComponent<DiceSide>().oppositeSide;
+        Debug.Log ("Rolled " + sideThatIsUp);
+        diceWhoseRollCompleted = null;
     }
 }
