0115f2105d8a7b31ac5aa970ffc5a2b3644f0354
Mathew Jennings <mathew.percussion@gmail.com>
2022-07-16 15:57:25 -0400

    When an enemy hits the player, knock the player back a bit

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
+
+    [SerializeField]
+    float knockBackForce;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Enemy")) {
             GetComponent<PlayerController>().TakeDamage();
+            GetKnockedBack(other.gameObject);
         }
+    }
+
+    private void GetKnockedBack(GameObject enemyThatKnockedBack)
+    {
+        Vector3 knockbackDirection = (transform.position - enemyThatKnockedBack.transform.position).normalized;
+        GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockBackForce);
     }
 }
